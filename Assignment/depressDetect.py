50164e79c61c9ac3889e8e0ea47fd56a52003a19
TAN CHUN HONG <tanchunhong717@gmail.com>
2022-06-15 23:22:50 +0800

    Adding comment

--- Assignment/depressDetect.py
+++ Assignment/depressDetect.py
@@ -6,20 +6,25 @@ from tensorflow.keras.backend import expand_dims
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-model = tf.keras.models.load_model("Assignment\depression.h5")
+#Load the model
+model = tf.keras.models.load_model("model\depression.h5")
 
 def deep_depression_detector(activity_data_csv_file):
+	#load the data
 	df = pd.read_csv(activity_data_csv_file)
+	#convert to numpy array
 	x = np.array([df['activity'].tolist()])
+	#add zero at behind so the data have same length with the longest data in data set
 	x = pad_sequences(
 		x, maxlen=65407, 
 		padding='post', 
 		truncating='post'
 	)
 
+	#Predict the result
 	y_pred = model.predict(x)
+	#Return the result
 	if y_pred >= 0.5:
 		return  {'prediction':'depressed'}
 	else:
 		return  {'prediction':'nondepressed'}
-
--- Assignment/trainingModel.py
+++ Assignment/trainingModel.py
@@ -7,24 +7,28 @@ from tensorflow.keras import layers
 from tensorflow.keras import models
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+#Create two python array to contain data
 X = []
 y = []
 
+#Load all the data from file and attach label to it according to their source
 for i in range(1, 24):
-	df = pd.read_csv("Assignment\data\condition\condition_{}.csv".format(i))
+	df = pd.read_csv("data\condition\condition_{}.csv".format(i))
 	x1 = np.array(df['activity'].tolist())
 	X.append(x1)
 	y.append(1)
 
 for i in range(1, 33):
-	df = pd.read_csv("Assignment\data\control\control_{}.csv".format(i))
+	df = pd.read_csv("data\control\control_{}.csv".format(i))
 	x1 = np.array(df['activity'].tolist())
 	X.append(x1)
 	y.append(0)
 
+#Convert the data to numpy array
 X = np.array(X)
 y = np.array(y)
 
+#Add zero behind to data so all data have same length
 X = pad_sequences(
 	X, 
 	maxlen=max([len(x1) for x1 in X]), 
@@ -32,8 +36,10 @@ X = pad_sequences(
 	truncating='post',
 )
 
+#Split the training set and validation set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
 
+#Construct a nueral network model
 model = models.Sequential([
 	layers.Dense(128, activation='relu', input_shape=X_train.shape[1:]),
 	layers.Dropout(0.3),
@@ -44,12 +50,14 @@ model = models.Sequential([
 	layers.Dense(1, activation='sigmoid')
 ])
 
+#Prepare for training the model, chose the algorithm, loss function, and metrics
 model.compile(
 	optimizer="adam",
 	loss="binary_crossentropy",
 	metrics=["binary_accuracy"],
 )
 
+#Training the model
 model.fit(
 	X_train,
 	y_train,
@@ -58,4 +66,5 @@ model.fit(
 	epochs=100,
 )
 
-model.save("depression.h5")
+#Save the model
+model.save("Assignment\model\depression.h5")
